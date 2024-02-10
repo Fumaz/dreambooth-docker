65d443cb02d5fe8657a8c91999b826d558a5cb4a
Fumaz <mail@fumaz.dev>
2024-02-10 13:35:18 +0100

    don't convert model automatically

--- script.sh
+++ script.sh
@@ -1,17 +1,7 @@
 # Configure accelerate
 accelerate config default
 
-# Download the model
-wget "$MODEL_URL" --content-disposition -O model.safetensors
-
-# Download the script to convert the model from safetensors to diffusers
-wget "https://raw.githubusercontent.com/huggingface/diffusers/v0.26.0/scripts/convert_original_stable_diffusion_to_diffusers.py"
-
-# Convert the model
-python convert_original_stable_diffusion_to_diffusers.py --checkpoint_path model.safetensors --dump_path SDModel/ --from_safetensors
-
 # Set environment variables
-export MODEL_NAME=SDModel
 export INSTANCE_DIR=faces
 export OUTPUT_DIR=$OUTPUT_MODEL
 
@@ -26,9 +16,9 @@ unzip dataset.zip -d $INSTANCE_DIR
 
 # Train the model
 accelerate launch train_dreambooth.py \
-      --pretrained_model_name_or_path=$MODEL_NAME  \
+      --pretrained_model_name_or_path="$MODEL_NAME"  \
       --instance_data_dir=$INSTANCE_DIR \
-      --output_dir=$OUTPUT_DIR \
+      --output_dir="$OUTPUT_DIR" \
       --instance_prompt="sks $INSTANCE_NAME" \
       --resolution=512 \
       --train_batch_size=1 \
